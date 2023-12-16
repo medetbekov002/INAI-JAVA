2519f8375eddbb2867ecf9579ea7761a58cc1994
theMIslam <islam.medetbekov.01@gmail.com>
2023-12-16 11:25:47 +0600

    added photo profile + normal code

--- app/src/main/java/com/london/inai_java/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/london/inai_java/ui/profile/ProfileFragment.java
@@ -2,69 +2,37 @@ package com.london.inai_java.ui.profile;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
-import com.london.inai_java.R;
-import com.london.inai_java.ui.profile.model.Cake;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import de.hdodenhof.circleimageview.CircleImageView;
+import com.london.inai_java.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
-    private CircleImageView ivProfile;
-    private TextView tvCakeName;
-    private TextView tvCakeDescription;
-    private ImageView btnArrow;
+    private FragmentProfileBinding binding;
 
-    private List<Cake> cakesList = new ArrayList<>();
-    private int currentCakeIndex = 0; // Index to keep track of the current cake displayed
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Initialize views
-        ivProfile = view.findViewById(R.id.iv_profile);
-        tvCakeName = view.findViewById(R.id.tv_napoleon); // Update ID to a more general name
-        tvCakeDescription = view.findViewById(R.id.tv_description);
-        btnArrow = view.findViewById(R.id.btn_arrow);
-
-        // Populate cakesList
-        cakesList.add(new Cake(
-                "Торт наполеон",
-                "Торт, по форме напоминающий двууголку...",
-                R.drawable.napoleon
-        ));
-        cakesList.add(new Cake(
-                "Торт Медовик",
-                "Медовый торт, изготовленный из слоёного теста...",
-                R.drawable.medovik
-        ));
-        // Add more cakes as needed
-
-        // Display initial cake details
-        displayCakeDetails(currentCakeIndex);
-
-        // Handle click on arrow to cycle through cakes
-        btnArrow.setOnClickListener(new View.OnClickListener() {
+        binding.btnArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentCakeIndex = (currentCakeIndex + 1) % cakesList.size(); // Cycle through cakes
-                displayCakeDetails(currentCakeIndex); // Update UI with new cake details
+                NavHostFragment.findNavController(ProfileFragment.this).navigateUp();
             }
         });
     }
-
-    private void displayCakeDetails(int index) {
-        Cake cake = cakesList.get(index);
-        ivProfile.setImageResource(cake.getImageResource());
-        tvCakeName.setText(cake.getName());
-        tvCakeDescription.setText(cake.getDescription());
-    }
 }
+
